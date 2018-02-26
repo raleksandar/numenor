16b8d0b451a4a77973668638b7a3d3646f285ab7
Aleksandar Ružičić <aleksandar@ruzicic.info>
2018-02-26 14:00:18 +0100

    Report lexeme instead of token type on errors

--- src/Parser/Error.ts
+++ src/Parser/Error.ts
@@ -5,12 +5,12 @@ function str(tokenType: TokenType.Any): string {
     return tokenType.toString().replace(/^.+?\((.+?)\)/, '$1');
 }
 
-export const UnknownToken = ({ type, line, col }: Token) => (
-    `Unexpected ${str(type)} at line ${line}:${col}`
+export const UnknownToken = ({ lexeme, line, col }: Token) => (
+    `Unexpected ${JSON.stringify(lexeme)} at line ${line}:${col}`
 );
 
-export const UnexpectedToken = (tokenType: TokenType.Any, { type, line, col }: Token) => (
-    `Expected ${str(tokenType)} but found ${str(type)} at line ${line}:${col}`
+export const UnexpectedToken = (tokenType: TokenType.Any, { lexeme, line, col }: Token) => (
+    `Expected ${str(tokenType)} but found ${JSON.stringify(lexeme)} at line ${line}:${col}`
 );
 
 export const InvalidLeftHandSide = `Invalid left-hand side in assignment`;
